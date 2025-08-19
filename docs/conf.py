771de9d271509e5e73b89469d97c0618307232ce
Paveljolak <paveljolak123@gmail.com>
2025-08-19 21:33:47 +0200

    main part of docs

---
+++ docs/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'docs'
+copyright = '2025, pavel'
+author = 'pavel'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+extensions = ['sphinx_multiversion']
+templates_path = ['_templates']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_sidebars = { '**': ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html', 'versions.html'] }
+smv_tag_whitelist = r'^.*$'
+smv_branch_whitelist = r'^(main|jazzy|humble)$'
+smv_outputdir_format = '{ref.name}'
+
+
---
+++ docs/index.rst
@@ -0,0 +1,9 @@
+This is the Main Part of the docs
+=================================
+
+This is the main part of the documentation.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
